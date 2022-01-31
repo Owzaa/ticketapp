c369f0f08eee6abbb8df2abdb40bf0bff71b5aa3
Owzaam <nyondo.olwethuu@gmail.com>
2022-01-31 12:56:30 +0200

    commit on master

--- ticketSite/ticketapp/serializers/TicketSerializer-API.py
+++ ticketSite/ticketapp/serializers/TicketSerializer-API.py
@@ -1,1 +1,11 @@
+from unittest.util import _MAX_LENGTH
+from django.forms import IntegerField
+from serializers import Ticket
+from ticketSite.ticketapp import serializers
 
+
+class TicketSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    ticket_id = serializers.CharField(_MAX_LENGTH=200)
+    title = serializers.CharField(_MAX_LENGTH=200)
+    content = serializers.TextField()
